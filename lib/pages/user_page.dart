4d57be96863ffedddf5bc2c418e6ea6bdebce37d
Julie Pražáková <xprazak6@mendelu.cz>
2024-05-20 15:36:03 +0200

    UserPage UI updated

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -13,61 +13,124 @@ class UserPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = FirebaseAuth.instance.currentUser;
+    final FirebaseAuth auth = FirebaseAuth.instance;
     final recipeProvider = ref.watch(recipeProviderState);
-    final userRecipes = recipeProvider.where((recipe) => recipe.userId == user?.uid).toList();
-    final favoriteRecipes = recipeProvider.where((recipe) => recipe.fav.contains(user?.uid)).toList();
+    final userRecipes =
+        recipeProvider.where((recipe) => recipe.userId == user?.uid).toList();
+    final favoriteRecipes = recipeProvider
+        .where((recipe) => recipe.fav.contains(user?.uid))
+        .toList();
 
     return Scaffold(
       appBar: const MyAppBar(),
       body: Column(
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: userRecipes.length,
-              itemBuilder: (context, index) {
-                final recipe = userRecipes[index];
-                return ListTile(
-                  title: Text(recipe.name),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      ref.read(recipeProviderState.notifier).deleteRecipe(recipe.id);
-                    },
+          Padding(
+            padding: const EdgeInsets.all(150.0),
+            child: Row(
+              children: [
+                const Icon(Icons.person, size: 60),
+                const SizedBox(width: 20),
+                Text(
+                  'Welcome user number ${user?.uid ?? 'Unknown'}!',
+                  style: const TextStyle(
+                    fontSize: 25,
                   ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RecipeDetailsPage(recipe: recipe),
-                      ),
-                    );
+                ),
+                const Expanded(
+                  child: SizedBox(height: 16),
+                ),
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.login),
+                  label: const Text('Logout'),
+                  onPressed: () async {
+                    await auth.signOut();
+                    Navigator.of(context).pushReplacementNamed('/');
                   },
-                );
-              },
+                ),
+              ],
             ),
           ),
-          const Divider(),
-          const Text(
-            'Favorite Recipes',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
+          SizedBox(height: 30),
           Expanded(
-            child: ListView.builder(
-              itemCount: favoriteRecipes.length,
-              itemBuilder: (context, index) {
-                final recipe = favoriteRecipes[index];
-                return ListTile(
-                  title: Text(recipe.name),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RecipePage(recipe: recipe),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'My Recipes',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                    );
-                  },
-                );
-              },
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: userRecipes.length,
+                        itemBuilder: (context, index) {
+                          final recipe = userRecipes[index];
+                          return ListTile(
+                            title: Text(recipe.name),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                ref
+                                    .read(recipeProviderState.notifier)
+                                    .deleteRecipe(recipe.id);
+                              },
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      RecipeDetailsPage(recipe: recipe),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+                const VerticalDivider(),
+                Expanded(
+                    child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Favorite Recipes',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: favoriteRecipes.length,
+                        itemBuilder: (context, index) {
+                          final recipe = favoriteRecipes[index];
+                          return ListTile(
+                            title: Text(recipe.name),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      RecipePage(recipe: recipe),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+              ],
             ),
           ),
         ],
