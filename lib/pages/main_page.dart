325fec9ffe1f7eae666243ed26ea454f4acfbc88
Julie Pražáková <xprazak6@mendelu.cz>
2024-05-20 10:36:08 +0200

    mainpage ui updated

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,109 +2,133 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:second_project/pages/recipe_page.dart';
-import '../models/categories.dart'; 
+import '../models/categories.dart';
 import './list_page.dart';
 import './category_page.dart';
 import '../providers/recipe_provider.dart';
 import '../providers/category_provider.dart';
-import 'app_bar.dart'; 
+import 'app_bar.dart';
 
 class MainPage extends ConsumerWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final recipeProvider = ref.watch(recipeProviderState); 
-    final pagingController = ref.watch(categoryProviderState); 
+    final recipeProvider = ref.watch(recipeProviderState);
+    final pagingController = ref.watch(categoryProviderState);
 
     return Scaffold(
       appBar: const MyAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            
-            // Display featured recipe
-            Container(
-              width: 300,
-              margin: const EdgeInsets.all(10),
-              child: recipeProvider.isEmpty
-                ? const Text('No recipes found')
-                : Card(
-                    margin: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Text(
-                            'Featured Recipe',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: InkWell(
+                onTap: () {
+                  if (recipeProvider.isNotEmpty) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) =>
+                                RecipePage(recipe: recipeProvider.first)));
+                  }
+                },
+                child: Container(
+                  height: 400,
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: recipeProvider.isEmpty
+                      ? const Center(child: Text('No recipes found'))
+                      : Card(
+                          margin: EdgeInsets.zero,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                title: Container(
+                                  width: double.infinity,
+                                  height: 300, // Placeholder image height
+                                  color: Colors.grey[300],
+                                ),
+                                subtitle: Text(recipeProvider.first.name,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 28,
+                                    )),
+                              ),
+                            ],
                           ),
-                          const SizedBox(height: 8), 
-                          ListTile(
-                            title: Text(recipeProvider.first.name),
-                            subtitle: Container(
-                              width: 80,
-                              height: 80,
-                              color: Colors.grey[300],
-                            ),
-                            onTap: () {
-                              Navigator.push(context, MaterialPageRoute(builder: (_) => RecipePage(recipe: recipeProvider.first)));
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                        ),
+                ),
+              ),
             ),
 
-            // Explore Categories section
-            Container(
-              width: 300,
-              margin: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
+            // Explore Categories button
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
                 children: [
-                  const SizedBox(height: 16),
-                  ElevatedButton(
+                  TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (_) => const CategoryPage()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => const CategoryPage()));
                     },
                     child: const Text(
                       'Explore All Categories',
                       style: TextStyle(
                         fontSize: 18,
+                        color: Colors
+                            .blue, // Change color to indicate it's clickable text
                       ),
                     ),
                   ),
-
-                  const SizedBox(height: 16), 
-
-                  // Display 3 categories
-                  PagedListView<int, Category>(
-                    pagingController: pagingController,
-                    builderDelegate: PagedChildBuilderDelegate<Category>(
-                      itemBuilder: (context, category, index) {
-                        return ListTile(
-                          title: Text(category.name),
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (_) => ListPage(category: category)));
-                          },
-                        );
-                      },
-                    ),
-                    shrinkWrap: true, // Pro snížení výšky na minimální nutnou hodnotu
-                    physics: NeverScrollableScrollPhysics(), // Zamezení skrolování v rámci PagedListView
-                  ),
                 ],
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Explore Categories section
+            SizedBox(
+              height: 200, // Adjust height as needed
+              child: PagedListView<int, Category>(
+                pagingController: pagingController,
+                builderDelegate: PagedChildBuilderDelegate<Category>(
+                  itemBuilder: (context, category, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => ListPage(category: category)));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 180, // Adjust image width
+                              height: 120, // Adjust image height
+                              color:
+                                  Colors.grey[300], // Placeholder image color
+                              // You can add actual images here
+                            ),
+                            const SizedBox(height: 8),
+                            Text(category.name,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                )),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                scrollDirection: Axis.horizontal,
               ),
             ),
           ],
